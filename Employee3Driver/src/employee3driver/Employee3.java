01fcd31744531708ad39a3a12f34204daa316433
Kevin Bell <bellKevin@pm.me>
2022-06-15 14:11:50 +0000

    Add files via upload

---
+++ Employee3Driver/src/employee3driver/Employee3.java
@@ -0,0 +1,10 @@
+package employee3driver;
+
+public class Employee3 {
+
+    public final String NAME;
+
+    public Employee3(String name) {
+        this.NAME = name;
+    }
+}
---
+++ Employee3Driver/src/employee3driver/Employee3Driver.java
@@ -0,0 +1,9 @@
+package employee3driver;
+
+public class Employee3Driver {
+
+    public static void main(String[] args) {
+        Employee3 computerProgrammer = new Employee3("Kevin Bell");
+        System.out.println(computerProgrammer.NAME);
+    }
+}
